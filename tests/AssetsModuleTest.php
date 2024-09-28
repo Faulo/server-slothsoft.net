c565ae3765b9b649895019874d193bc02fff59d3
Daniel Schulz <faulolio@gmail.com>
2024-09-28 22:36:47 +0200

    build: update files

--- tests/AssetsModuleTest.php
+++ tests/AssetsModuleTest.php
@@ -1,12 +1,13 @@
 <?php
 declare(strict_types = 1);
+namespace Slothsoft\Server\Slothsoft\Tests;
 
 use Slothsoft\Farah\FarahUrl\FarahUrlAuthority;
 use Slothsoft\Farah\ModuleTests\AbstractModuleTest;
 
-class AssetsModuleTest extends AbstractModuleTest
-{
-    protected static function getManifestAuthority() : FarahUrlAuthority {
-        return FarahUrlAuthority::createFromVendorAndModule('slothsoft', 'slothsoft.net');
+class AssetsModuleTest extends AbstractModuleTest {
+
+    protected static function getManifestAuthority(): FarahUrlAuthority {
+        return FarahUrlAuthority::createFromVendorAndModule('slothsoft', 'test.slothsoft.net');
     }
 }
--- tests/SitemapTest.php
+++ tests/SitemapTest.php
@@ -1,13 +1,14 @@
 <?php
 declare(strict_types = 1);
+namespace Slothsoft\Server\Slothsoft\Tests;
 
 use Slothsoft\Farah\Configuration\AssetConfigurationField;
 use Slothsoft\Farah\ModuleTests\AbstractSitemapTest;
 use Slothsoft\Farah\Module\Asset\AssetInterface;
 
-class SitemapTest extends AbstractSitemapTest
-{
-    protected static function loadSitesAsset() : AssetInterface {
-        return (new AssetConfigurationField('farah://slothsoft@slothsoft.net/sitemap'))->getValue();
+class SitemapTest extends AbstractSitemapTest {
+
+    protected static function loadSitesAsset(): AssetInterface {
+        return (new AssetConfigurationField('farah://slothsoft@test.slothsoft.net/sitemap'))->getValue();
     }
 }

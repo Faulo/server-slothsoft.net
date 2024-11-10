7a21090b7fc902dfd0aae3996a7312974067f67d
Daniel Schulz <faulolio@gmail.com>
2024-11-10 18:45:01 +0100

    fix: sitemap

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -15,11 +15,11 @@ ServerEnvironment::setCacheDirectory($root . DIRECTORY_SEPARATOR . 'cache');
 ServerEnvironment::setLogDirectory($root . DIRECTORY_SEPARATOR . 'log');
 ServerEnvironment::setDataDirectory($root . DIRECTORY_SEPARATOR . 'data');
 
-Kernel::setCurrentSitemap('farah://slothsoft@test.slothsoft.net/sitemap');
+Kernel::setCurrentSitemap('farah://slothsoft@slothsoft.net/sitemap');
 Kernel::setTrackingEnabled(false);
 Dictionary::setSupportedLanguages('en-us');
 
-Module::registerWithXmlManifestAndDefaultAssets('slothsoft@test.slothsoft.net', $root . DIRECTORY_SEPARATOR . 'assets');
+Module::registerWithXmlManifestAndDefaultAssets('slothsoft@slothsoft.net', $root . DIRECTORY_SEPARATOR . 'assets');
 
 if ($file = getenv('MYSQL_ROOT_PASSWORD_FILE') and $password = file_get_contents($file)) {
     Client::setDefaultAuthority(new Authority('mysql', 'root', $password));

95c4f6b5e127239faba4a3d8ab8d70f65b89418c
Daniel Schulz <faulolio@gmail.com>
2025-09-29 15:42:11 +0200

    feat: whatthehell

--- src/Assets/WhatTheHell/PushBuilder.php
+++ src/Assets/WhatTheHell/PushBuilder.php
@@ -6,12 +6,13 @@ use Slothsoft\Farah\FarahUrl\FarahUrlArguments;
 use Slothsoft\Farah\Module\Asset\AssetInterface;
 use Slothsoft\Farah\Module\Asset\ExecutableBuilderStrategy\ExecutableBuilderStrategyInterface;
 use Slothsoft\Farah\Module\Executable\ExecutableStrategies;
+use Slothsoft\Farah\Module\Executable\ResultBuilderStrategy\NullResultBuilder;
 
-class PushBuilder implements ExecutableBuilderStrategyInterface
-{
-
-    public function buildExecutableStrategies(AssetInterface $context, FarahUrlArguments $args): ExecutableStrategies
-    {}
+class PushBuilder implements ExecutableBuilderStrategyInterface {
+    
+    public function buildExecutableStrategies(AssetInterface $context, FarahUrlArguments $args): ExecutableStrategies {
+        return new ExecutableStrategies(new NullResultBuilder());
+    }
     /*
      * if (! isset($_REQUEST['type'])) {
      * $_REQUEST['type'] = 'message';

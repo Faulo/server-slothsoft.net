228698a63833801a7cc25619b065aec07f906cee
Daniel Schulz (Estelle) <faulolio@gmail.com>
2026-01-01 19:51:52 +0100

    refactor(tests): lint

--- tests/AssetsManifestTest.php
+++ tests/AssetsManifestTest.php
@@ -5,7 +5,7 @@ namespace Slothsoft\Server\Slothsoft\Tests;
 use Slothsoft\FarahTesting\Module\AbstractXmlManifestTest;
 
 class AssetsManifestTest extends AbstractXmlManifestTest {
-
+    
     protected static function getManifestDirectory(): string {
         return dirname(__DIR__) . DIRECTORY_SEPARATOR . 'assets';
     }
--- tests/SitemapTest.php
+++ tests/SitemapTest.php
@@ -7,7 +7,7 @@ use Slothsoft\FarahTesting\Module\AbstractSitemapTest;
 use Slothsoft\Farah\Module\Asset\AssetInterface;
 
 class SitemapTest extends AbstractSitemapTest {
-
+    
     protected static function loadSitesAsset(): AssetInterface {
         return (new AssetConfigurationField('farah://slothsoft@slothsoft.net/sitemap'))->getValue();
     }

63e9ed39d6efce95a3258ded61abc01b10b41c44
Daniel Schulz (Estelle) <faulolio@gmail.com>
2026-01-01 19:52:57 +0100

    refactor(tests): fix

--- tests/SitemapTest.php
+++ tests/SitemapTest.php
@@ -2,11 +2,11 @@
 declare(strict_types = 1);
 namespace Slothsoft\Server\Slothsoft;
 
-use Sluse 
-Slothsoft\FarahTesting\Module\AbstractSitemapTest;
+use Slothsoft\FarahTesting\Module\AbstractSitemapTest;
 use Slothsoft\Farah\Configuration\AssetConfigurationField;
 use Slothsoft\Farah\Module\Asset\AssetInterface;
-l class SitemapTest extends AbstractSitemapTest {
+
+final class SitemapTest extends AbstractSitemapTest {
     
     protected static function loadSitesAsset(): AssetInterface {
         return (new AssetConfigurationField('farah://slothsoft@slothsoft.net/sitemap'))->getValue();

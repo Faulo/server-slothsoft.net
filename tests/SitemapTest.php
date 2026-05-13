309adc309b8239e209fe770a86c7290211cebe55
Daniel Schulz <faulolio@gmail.com>
2026-05-13 15:39:40 +0200

    build: update files

--- tests/SitemapTest.php
+++ tests/SitemapTest.php
@@ -10,6 +10,6 @@ use Slothsoft\FarahTesting\Module\AbstractSitemapTest;
 final class SitemapTest extends AbstractSitemapTest {
     
     protected static function loadSitesAsset(): AssetInterface {
-        return (new AssetConfigurationField('farah://slothsoft@archive.slothsoft.net/sitemap'))->getValue();
+        return (new AssetConfigurationField('farah://slothsoft@slothsoft.net/sitemap'))->getValue();
     }
 }
